1c97e0744a238ae2bd6492a6e59e2a63216e24c3
guohualin <824430898@qq.com>
2022-10-08 10:50:27 +0000

    init

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/mine/rust-playground/guessing_game/target/debug/guessing_game: /home/mine/rust-playground/guessing_game/src/main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/mine/rust-playground/hello_cargo/target/debug/hello_cargo: /home/mine/rust-playground/hello_cargo/src/main.rs
