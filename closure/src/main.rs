35cfb43ac2407ad89b5be1e31e4b7066b710df72
guohualin <824430898@qq.com>
2023-01-16 07:47:44 +0000

    feat(closure): 复习闭包

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,17 +1,29 @@
 fn main() {
-    clo(|| {
-        go; // 这里只会warn，不会执行，path statement with no effect(无效声明)
+    closure_fn(|| {
+        // 这里只会warn，不会执行，path statement with no effect(无效声明)
+        closure;
     });
-    clo(|| {
-        go();
+    closure_fn(|| {
+        closure();
     });
-    clo(go); // 思考与第5行区别是什么？
+    closure_fn(closure); // 思考与第5行区别是什么？
+
+    closure_fn(|| closure2(1_u8));
+    //下面代码会报错,对应第8行代码
+    //expected a `FnOnce<()>` closure, found `()`
+    //the trait `FnOnce<()>` is not implemented for `()`
+    //wrap the `()` in a closure with no arguments: `|| { /* code */ }`
+    //clo(closure2(1_u8))
 }
 
-fn go() {
+fn closure() {
     println!("go!");
 }
 
-fn clo<F: FnOnce()>(closure_fn: F) {
+fn closure2(param: u8) {
+    println!("go!param is :{:?}", param);
+}
+
+fn closure_fn<F: FnOnce()>(closure_fn: F) {
     closure_fn();
 }
