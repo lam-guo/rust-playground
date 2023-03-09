e5657679d4bbd7c52f5e4ef4cde8958b2f0f4070
guohualin <824430898@qq.com>
2023-03-09 10:34:01 +0000

    feat(tokio_web): webserver tokio version

---
+++ webserver-tokio/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("0.0.0.0:8888").await.unwrap();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        handle_connection(stream).await;
+    }
+}
+
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).await.unwrap();
+
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).await.unwrap();
+}
