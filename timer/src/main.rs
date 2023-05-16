54ccc40c971d4e9495e3fde45f34fc06f1e2df6f
guohualin <824430898@qq.com>
2023-05-16 09:57:20 +0000

    feat(select): tokio::select!机制留下问题

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -1,13 +1,14 @@
 use std::ops::Add;
 use std::time::Duration;
-use tokio::time::Instant;
+use tokio::time::{self, Instant};
 
 #[tokio::main]
 async fn main() {
     let mut next_record_at = Instant::now().add(Duration::from_secs(20));
+
     loop {
         tokio::select! {
-            _ = tokio::time::sleep_until(next_record_at) => {
+            _ = time::sleep_until(next_record_at) => {
                 println!("inside sleep_until------------------------,{:?}",next_record_at);
                 next_record_at = Instant::now().add(Duration::from_secs(20));
             }
@@ -20,10 +21,23 @@ async fn main() {
             //     next_record_at = Instant::now().add(Duration::from_secs(4));
             //     println!("inside a------------------------");
             // }
+            _ =b() =>{
+                println!("inside b------------------------");
+            }
         }
     }
 }
 
 async fn a() {
+    println!("into a------------------------");
     tokio::time::sleep(Duration::from_secs(5)).await;
+    println!("out a------------------------");
+}
+
+// b比a慢，只会输出into b，不会输出out b
+// TODO 了解下为什么out b不输出？
+async fn b() {
+    println!("into b------------------------");
+    tokio::time::sleep(Duration::from_secs(6)).await;
+    println!("out b------------------------");
 }
